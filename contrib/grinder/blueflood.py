56302758593e2ec579e4e30a76530d7e5f33768e
George Jahad <github@blackbirdsystems.net>
2015-06-15 17:40:30 -0700

    basic tests working

--- contrib/grinder/blueflood.py
+++ contrib/grinder/blueflood.py
@@ -38,7 +38,7 @@ stage_config = {
   'multiplot_per_minute': 20,
   'singleplot_per-minute': 300}
 
-default_config = stage_config
+default_config = qe01_config
 
 RAND_MAX =  982374239
 
@@ -52,6 +52,10 @@ class ThreadManager(object):
   def prn_types(self):
     print self.types
 
+  @staticmethod
+  def teststat():
+    print("g1")
+  
   def create_all_batches(self, agent_number):
     for x in self.types:
       x.create_batches(agent_number)
@@ -87,6 +91,12 @@ class ThreadType(object):
 
 class IngestThread(ThreadType):
   batches = []
+  units_map = {0: 'minutes',
+               1: 'hours',
+               2: 'days',
+               3: 'months',
+               4: 'years',
+               5: 'decades'}
 
   @classmethod
   def create_batches(cls, agent_number):
@@ -96,91 +106,85 @@ class IngestThread(ThreadType):
                                             default_config['num_nodes'])
 
   def __init__(self, thread_num):
-    start, end = generate_job_range(len(self.batches), 
+    start, end = self.generate_job_range(len(self.batches), 
                                     default_config['ingest_concurrency'], thread_num)
-    self.slice = batches[start:end]
+    self.slice = self.batches[start:end]
     self.position = 0
     self.finish_time = int(time.time()) + (default_config['report_interval'] / 1000)
 
   def num_threads(self):
     return default_config['ingest_concurrency']
 
-def generate_metric_name(metric_id):
-  return default_config['name_fmt'] % metric_id
+  def generate_metric_name(self, metric_id):
+    return default_config['name_fmt'] % metric_id
 
-units_map = {0: 'minutes',
-             1: 'hours',
-             2: 'days',
-             3: 'months',
-             4: 'years',
-             5: 'decades'}
+  def generate_unit(self, tenant_id):
+    unit_number = tenant_id % 6
+    return units_map[unit_number]
 
-def generate_unit(tenant_id):
-  unit_number = tenant_id % 6
-  return units_map[unit_number]
+  @staticmethod
+  def divide_batches(metrics, batch_size):
+    b = []
+    for i in range(0, len(metrics), batch_size):
+      b.append(metrics[i:i+batch_size])
+    return b
 
-def divide_batches(metrics, batch_size):
-  batches = []
-  for i in range(0, len(metrics), batch_size):
-    batches.append(metrics[i:i+batch_size])
-  return batches
+  @staticmethod
+  def generate_job_range(total_jobs, total_servers, server_num):
+    jobs_per_server = total_jobs/total_servers
+    remainder = total_jobs % total_servers
+    start_job = jobs_per_server * server_num
+    start_job += min(remainder, server_num)
+    end_job = start_job + jobs_per_server
+    if server_num < remainder:
+      end_job += 1
+    return (start_job, end_job)
 
-def generate_job_range(total_jobs, total_servers, server_num):
-  jobs_per_server = total_jobs/total_servers
-  remainder = total_jobs % total_servers
-  start_job = jobs_per_server * server_num
-  start_job += min(remainder, server_num)
-  end_job = start_job + jobs_per_server
-  if server_num < remainder:
-    end_job += 1
-  return (start_job, end_job)
+  @staticmethod
+  def generate_metrics_tenants(batch_size, tenant_ids, metrics_per_tenant, agent_number, num_nodes):
+    def generate_metrics_for_tenant(tenant_id):
+      l = [];
+      for x in range(metrics_per_tenant):
+        l.append([tenant_id, x])
+      return l
+    tenants_in_shard = range(*self.generate_job_range(tenant_ids, num_nodes, agent_number))
+    metrics = []
+    for y in map(generate_metrics_for_tenant, tenants_in_shard):
+      metrics += y
+    random.shuffle(metrics)
+    return divide_batches(metrics, batch_size)
 
-def generate_metrics_tenants(batch_size, tenant_ids, metrics_per_tenant, agent_number, num_nodes):
-  def generate_metrics_for_tenant(tenant_id):
-    l = [];
-    for x in range(metrics_per_tenant):
-      l.append([tenant_id, x])
-    return l
-  tenants_in_shard = range(*generate_job_range(tenant_ids, num_nodes, agent_number))
-  metrics = []
-  for y in map(generate_metrics_for_tenant, tenants_in_shard):
-    metrics += y
-  random.shuffle(metrics)
-  return divide_batches(metrics, batch_size)
+  def generate_metric(self, time, tenant_id, metric_id):
+    return {'tenantId': str(tenant_id),
+            'metricName': self.generate_metric_name(metric_id),
+            'unit': self.generate_unit(tenant_id),
+            'metricValue': random.randint(0, RAND_MAX),
+            'ttlInSeconds': (2 * 24 * 60 * 60),
+            'collectionTime': time}
 
-def generate_metric(time, tenant_id, metric_id):
-  return {'tenantId': str(tenant_id),
-          'metricName': generate_metric_name(metric_id),
-          'unit': generate_unit(tenant_id),
-          'metricValue': random.randint(0, RAND_MAX),
-          'ttlInSeconds': (2 * 24 * 60 * 60),
-          'collectionTime': time}
+  def generate_payload(self, time, batch):
+    payload = map(lambda x:self.generate_metric(time,*x), batch)
+    return json.dumps(payload)
 
-def generate_payload(time, batch):
-  payload = map(lambda x:generate_metric(time,*x), batch)
-  return json.dumps(payload)
-
-def ingest_url():
-  return "%s/v2.0/tenantId/ingest/multi" % default_config['url']
+  def ingest_url(self):
+    return "%s/v2.0/tenantId/ingest/multi" % default_config['url']
 
 
-def make_request(self, logger, request_handler):
-  if self.position >= len(self.slice):
-    self.position = 0
-    sleep_time = self.finish_time - int(time.time())
-    self.finish_time += (default_config['report_interval'] / 1000)
-    if sleep_time < 0:
-      #return error
-      logger("finish time error")
-    else:
-      logger("pausing for %d" % sleep_time)
-      time.sleep(sleep_time)
-  payload = generate_payload(int(time.time()),
-                                         self.slice[self.position])
-  self.position += 1
-  result = request_handler.POST(ingest_url(), payload)
-  return result
+  def make_request(self, logger, request_handler):
+    if self.position >= len(self.slice):
+      self.position = 0
+      sleep_time = self.finish_time - int(time.time())
+      self.finish_time += (default_config['report_interval'] / 1000)
+      if sleep_time < 0:
+        #return error
+        logger("finish time error")
+      else:
+        logger("pausing for %d" % sleep_time)
+        time.sleep(sleep_time)
+    payload = self.generate_payload(int(time.time()),
+                                           self.slice[self.position])
+    self.position += 1
+    result = request_handler.POST(self.ingest_url(), payload)
+    return result
 
 ThreadManager.add_type(IngestThread)
-
-  
